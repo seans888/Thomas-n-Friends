685c76baef4640da58ec19779a2885dd4cc8f167
jobiedelacruz <jobie_17_cute@yahoo.com>
2017-10-01 21:20:41 +0800

    FireApp2

--- 3 - CSPROJ FILES/PROTOTYPE/FireApp/app/src/main/java/com/example/jobie/fireapp/MainActivity.java
+++ 3 - CSPROJ FILES/PROTOTYPE/FireApp/app/src/main/java/com/example/jobie/fireapp/MainActivity.java
@@ -10,28 +10,12 @@ import com.google.firebase.crash.internal.service.FirebaseCrashSenderService;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mSendData;
 
-    private FirebaseApp mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.getApps(this);
 
-
-        mRef = Firebase("https://fir-app-62ce1.firebaseio.com/");
-
-        mSendData = (Button) findViewById(R.id.sendData);
-
-        mSendData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                FirebaseApp mRefChild = mRef.getName("Name");
-
-            }
-        });
 
     }
 
