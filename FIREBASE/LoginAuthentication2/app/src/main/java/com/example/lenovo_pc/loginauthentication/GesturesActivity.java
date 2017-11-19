a402729d1d8b8b189ee2d456e228b00ad5ea3c74
mpjimenez0 <cramjimenez07@gmail.com>
2017-11-19 12:28:34 +0800

    Added additional view (for the Main Menu)

---
+++ FIREBASE/LoginAuthentication2/app/src/main/java/com/example/lenovo_pc/loginauthentication/GesturesActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo_pc.loginauthentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GesturesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gestures_activity);
+    }
+}
