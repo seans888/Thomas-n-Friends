771dd88cf2ff6f4ee23fc5b7ea47994461f7b905
jobiedelacruz <jobie_17_cute@yahoo.com>
2017-12-19 21:32:51 +0800

    Authentication

---
+++ FIREBASE/LoginAuthentication2/Spinner/app/src/main/java/com/example/jobie/spinner/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jobie.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner3);
+
+        ArrayAdapter.createFromResource()
+    }
+}
